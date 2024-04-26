0bb5afaf3ffd56da588206f2989be29b92e966bf
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 22:41:19 +0200

    Fix homepage styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, Typography, List, ListItem, rgbToHex } from '@mui/material';
+import { Box, Typography, List, ListItem, Button } from '@mui/material';
 
 function App() {
   return (
@@ -6,16 +6,32 @@ function App() {
     <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden', gap: -10 }}>
       <Box sx={{ position: 'relative' }}>
         <Box sx={{ marginLeft: 10 }}>
-        <Typography sx={{ fontSize: 100, marginBottom: 55, color: '#818181' }}>
+        <Typography sx={{ fontSize: 100, marginBottom: 55, color: '#818181', marginRight: -16 }}>
           Håkon
         </Typography>
         </Box>
       </Box>
-      <List sx={{ fontSize: 25, color: '#818181'}}>
-        <ListItem>Projects</ListItem>
-        <ListItem>Blog</ListItem>
-        <ListItem>Me</ListItem>
-        <ListItem>Contact</ListItem>
+      <List>
+        <ListItem>
+          <Button sx={{ color: '#818181', fontWeight: 'bolder', fontSize: 20 }}>
+            Projects
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button sx={{ color: '#818181', fontWeight: 'bolder', fontSize: 20 }}>
+            Blog
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button sx={{ color: '#818181', fontWeight: 'bolder', fontSize: 20, marginLeft: -1 }}>
+           Me
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button sx={{ color: '#818181', fontWeight: 'bolder', fontSize: 20 }}>
+            Contact
+          </Button>
+        </ListItem>
       </List>
       <Typography
         sx={{
@@ -23,7 +39,7 @@ function App() {
           position: 'absolute',
           color: 'white',
           top: '28%',
-          left: '35%', 
+          left: '29%', 
           transform: 'translate(-50%, -50%)',
         }}
       >
