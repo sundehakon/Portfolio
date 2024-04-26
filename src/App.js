fdaee5fb4c4650147254366857797c65f4378b4d
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 18:22:49 +0200

    Add homepage elements to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import { Box, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Box>
-      
+    <Box sx={{ display: 'flex', gap: 100 }}>
+      <Typography>Håkon Sunde</Typography>
+      <img src='/images/IMG_0904.jpeg' alt='Blossoming nature' style={{ width: 400, height: 300 }}/>
     </Box>
   );
 }
