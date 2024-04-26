619da21f380072c5119b74e44a3f30424f6430f4
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 18:51:59 +0200

    Fix styling and positioning of homepage elements

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import { Box, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', gap: 60 }}>
-      <Typography>Håkon Sunde</Typography>
-      <img src='/images/IMG_0903.jpeg' alt='Blossoming nature' style={{ width: '150vw', height: '100vh' }}/>
+    <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden', gap: 3 }}>
+      <Typography sx={{ fontSize: 100, marginLeft: 12, marginBottom: 55 }}>
+        Håkon
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: 100,
+          position: 'absolute',
+          color: 'white',
+          top: '28%',
+          left: '30%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        Sunde
+      </Typography>
+      <img
+        src='/images/IMG_0903.jpeg'
+        alt='Blossoming nature'
+        style={{ width: 'auto', height: '100%', minWidth: '80%' }}
+      />
     </Box>
   );
 }
