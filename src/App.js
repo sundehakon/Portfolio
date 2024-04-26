65fd89821218960fcc1b36f4961240468b0647d3
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 22:33:16 +0200

    Change color for header and list text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Box, Typography, List, ListItem } from '@mui/material';
+import { Box, Typography, List, ListItem, rgbToHex } from '@mui/material';
 
 function App() {
   return (
     <div>
-    <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden' }}>
-      <Typography sx={{ fontSize: 100, marginLeft: 12, marginBottom: 55 }}>
-        Håkon
-      </Typography>
-      <List>
+    <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden', gap: -10 }}>
+      <Box sx={{ position: 'relative' }}>
+        <Box sx={{ marginLeft: 10 }}>
+        <Typography sx={{ fontSize: 100, marginBottom: 55, color: '#818181' }}>
+          Håkon
+        </Typography>
+        </Box>
+      </Box>
+      <List sx={{ fontSize: 25, color: '#818181'}}>
         <ListItem>Projects</ListItem>
         <ListItem>Blog</ListItem>
         <ListItem>Me</ListItem>
@@ -19,7 +23,7 @@ function App() {
           position: 'absolute',
           color: 'white',
           top: '28%',
-          left: '33%', 
+          left: '35%', 
           transform: 'translate(-50%, -50%)',
         }}
       >
@@ -28,7 +32,7 @@ function App() {
       <img
         src='/images/IMG_0903.jpeg'
         alt='Blossoming nature'
-        style={{ width: 'auto', height: '100%', minWidth: '80%' }}
+        style={{ width: 'auto', height: '100%', minWidth: '83%' }}
       />
     </Box>
     </div>
