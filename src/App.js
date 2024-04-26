d523a79706443cec89a7b91017b64401daf854fe
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 18:30:04 +0200

    Fix homepage image positioning

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Box, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', gap: 100 }}>
+    <Box sx={{ display: 'flex', gap: 60 }}>
       <Typography>Håkon Sunde</Typography>
-      <img src='/images/IMG_0904.jpeg' alt='Blossoming nature' style={{ width: 400, height: 300 }}/>
+      <img src='/images/IMG_0903.jpeg' alt='Blossoming nature' style={{ width: '150vw', height: '100vh' }}/>
     </Box>
   );
 }
