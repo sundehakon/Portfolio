3d7aca7e0e6eb5de33b0af9eef5347b0e8ed7da4
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-26 19:07:17 +0200

    Add nav list to homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, List, ListItem } from '@mui/material';
 
 function App() {
   return (
-    <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden', gap: 3 }}>
+    <div>
+    <Box sx={{ position: 'relative', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', overflow: 'hidden' }}>
       <Typography sx={{ fontSize: 100, marginLeft: 12, marginBottom: 55 }}>
         Håkon
       </Typography>
+      <List>
+        <ListItem>Projects</ListItem>
+        <ListItem>Blog</ListItem>
+        <ListItem>Me</ListItem>
+        <ListItem>Contact</ListItem>
+      </List>
       <Typography
         sx={{
           fontSize: 100,
           position: 'absolute',
           color: 'white',
           top: '28%',
-          left: '30%',
+          left: '33%', 
           transform: 'translate(-50%, -50%)',
         }}
       >
@@ -24,7 +31,9 @@ function App() {
         style={{ width: 'auto', height: '100%', minWidth: '80%' }}
       />
     </Box>
+    </div>
   );
 }
 
 export default App;
+
