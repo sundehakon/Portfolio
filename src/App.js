73e7d537808341c453df172be635fd03d9a87133
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-29 10:34:24 +0200

    Add project card for Random Number Game

--- src/App.js
+++ src/App.js
@@ -111,16 +111,16 @@ function App() {
       <Card variant='outlined' sx={{ maxWidth: 346 }}>
         <CardMedia 
           sx={{ height: 280 }}
-          image='/images/havblikk-logo.png'
+          image='/images/numbers.webp'
           title='Logo for Havblikk'
         />
-        <hr style={{ width: '90%' }}/> 
         <CardContent>
-          <Typography variant='h5'>Hotel Website</Typography>
-          <Typography variant='subtitle2' sx={{ marginTop: 1 }}>This was created as a project for my programming class at school. It is designed to make hotel reservations easy both for the customer and staff. It aims to provide information about the hotel, meanwhile also providing functionality which makes the hotel experience that much better!</Typography>
+          <Typography variant='h5'>Random Number Game</Typography>
+          <Typography variant='subtitle2' sx={{ marginTop: 1 }}>A game created using C# which main purpose is having the user guess a computer generated number. This game provides functionality such as letting the user pick a difficulty, randomly generating numbers that users can guess, a score which checks your difference from the actual answer and ASCII art representing the game intro, game over and victory!</Typography>
         </CardContent>
         <CardActions sx={{ justifyContent: 'center' }}>
-          <Button size='small' href='https://github.com/sundehakon/Hotell-React' target='_blank'>Learn More</Button>
+          <Button size='small' href='https://github.com/sundehakon/random-number-game' target='_blank'>Learn More</Button>
+          <Button size='small' href='https://dotnetfiddle.net/ZtSWcW' target='_blank'>Try It Out</Button>
         </CardActions>
       </Card>
     </Grid>
