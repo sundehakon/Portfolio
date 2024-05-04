b18bed421ae1541c9b4c9a1c1a73f66c9b4d40e0
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-04 21:23:53 +0200

    Successful implementation of database fetch

--- server.js
+++ server.js
@@ -4,7 +4,32 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const app = express();
 app.use(cors());
 app.use(bodyParser.json()); 
 
 mongoose.connect(process.env.MONGO_URI, {});
+
+const postSchema = new mongoose.Schema({
+    userId: String,
+    title: String,
+    content: String,
+    date: Date,
+}, { collection: 'Blog' });
+
+const Post = mongoose.model('Post', postSchema);
+
+app.get('/api/Blog', async (req, res) => {
+    try {
+        const blog = await Post.find();
+        res.status(200).json(blog);
+    } catch (error) {
+        console.error('Error fetching post:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+const PORT = 9999;
+app.listen(PORT, () => {
+    console.log(`PORT: ${PORT}`);
+})
