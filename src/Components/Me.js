9a8e0f6c63c76fcff77d59ca7e69b95d7a27f8cb
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-09 00:10:50 +0200

    Make GitHub logo white for contrast style purposes

--- src/Components/Me.js
+++ src/Components/Me.js
@@ -13,7 +13,7 @@ const Me = () => {
             <Typography sx={{ marginTop: 2 }}>
             Want to talk with me?: <Link href='mailto: hakon.su@gmail.com' underline='hover' color={'inherit'}>hakon.su@gmail.com</Link>
             </Typography>
-            <a href='https://github.com/sundehakon' target='_blank' rel='noreferrer'><img src='https://raw.githubusercontent.com/jmnote/z-icons/master/svg/github.svg' alt='GitHub logo' style={{ height: 50, marginTop: 20 }}/></a>
+            <a href='https://github.com/sundehakon' target='_blank' rel='noreferrer'><img src='/images/github-white.png' alt='GitHub logo' style={{ height: 50, marginTop: 20 }}/></a>
             <a href='https://twitter.com/lordsunde' target='_blank' rel='noreferrer'><img src='https://raw.githubusercontent.com/jmnote/z-icons/master/svg/twitter.svg' alt='Twitter logo' style={{ height: 50, marginTop: 20, marginLeft: 20 }}/></a>
             <a href='https://www.facebook.com/hakon.sunde.313/' target='_blank' rel='noreferrer'><img src='https://raw.githubusercontent.com/jmnote/z-icons/master/svg/facebook.svg' alt='Facebook logo' style={{ height: 50, marginTop: 20, marginLeft: 20 }}/></a>
         </Box>
