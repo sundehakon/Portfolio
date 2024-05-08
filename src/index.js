1f3173784384a0af942cd78d4de2e8e7739f33cc
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-08 19:16:21 +0200

    Fix minor issues in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -20,8 +20,7 @@ root.render(
     cacheLocation='localstorage'
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
