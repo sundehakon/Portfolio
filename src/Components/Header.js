03428959952a96ce92c2de3e6d702df1d1fa8f5c
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-09 00:06:55 +0200

    Fix and add code for rwd on homepage

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,37 @@
-import { Box, List, ListItemButton } from '@mui/material';
+import { Box, List, ListItemButton, Typography, useMediaQuery, useTheme } from '@mui/material';
 
 const Header = () => {
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', padding: 3 }}>
-            <h1 className='caprasimo-regular'>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: isSmallScreen ? 'center' : 'flex-start',
+                padding: isSmallScreen ? '1rem 3rem 3rem' : '3rem',
+                flexDirection: isSmallScreen ? 'column' : 'row',
+                textAlign: isSmallScreen ? 'center' : 'left', 
+            }}
+        >
+            <h1
+                className='caprasimo-regular'
+                style={{
+                    color: '#fcfcfc',
+                    fontSize: isSmallScreen ? '3rem' : '4.375rem', 
+                    marginBottom: isSmallScreen ? '1rem' : '0', 
+                    marginTop: isSmallScreen ? '0' : '0rem',
+                }}
+            >
                 Håkon Sunde
             </h1>
-            <List sx={{ display: 'flex', color: '#818181', border: 'solid 1px #818181', borderRadius: 5 }}>
-                <ListItemButton sx={{ '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Me</ListItemButton>
-                <ListItemButton sx={{ '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Projects</ListItemButton>
-                <ListItemButton sx={{ '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }}>User</ListItemButton>
-                <ListItemButton sx={{ '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Blog</ListItemButton>
-                <ListItemButton sx={{ '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Contact</ListItemButton>
+            <List sx={{ display: 'flex', color: '#818181', flexDirection: isSmallScreen ? 'column' : 'row' }}>
+                <ListItemButton sx={{ '&:hover': { color: '#fcfcfc' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Me</ListItemButton>
+                <ListItemButton sx={{ '&:hover': { color: '#fcfcfc' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Projects</ListItemButton>
+                <ListItemButton sx={{ '&:hover': { color: '#fcfcfc' } }} disableRipple style={{ backgroundColor: 'transparent' }}>User</ListItemButton>
+                <ListItemButton sx={{ '&:hover': { color: '#fcfcfc' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Blog</ListItemButton>
+                <ListItemButton sx={{ '&:hover': { color: '#fcfcfc' } }} disableRipple style={{ backgroundColor: 'transparent' }}>Contact</ListItemButton>
             </List>
         </Box>
     );
--- src/Components/Me.js
+++ src/Components/Me.js
@@ -2,13 +2,13 @@ import { Box, Typography, Link } from '@mui/material'
 
 const Me = () => {
     return (
-        <Box sx={{  display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', padding: 10, paddingBottom: 34 }}>
-        <Box sx={{ color: '#818181' }}>
+        <Box sx={{  display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', padding: 10, paddingBottom: 34, textAlign: 'center' }}>
+        <Box sx={{ color: '#fcfcfc' }}>
             <Typography sx={{ fontWeight: 'bold' }} variant='h4'>
-            Hi, I'm Håkon Sunde
+            Hi, there!
             </Typography>
             <Typography sx={{ marginTop: 2 }}>
-            a 16 year old developer from Norway
+            I'm a 16 year old developer from Norway
             </Typography>
             <Typography sx={{ marginTop: 2 }}>
             Want to talk with me?: <Link href='mailto: hakon.su@gmail.com' underline='hover' color={'inherit'}>hakon.su@gmail.com</Link>
@@ -17,7 +17,6 @@ const Me = () => {
             <a href='https://twitter.com/lordsunde' target='_blank' rel='noreferrer'><img src='https://raw.githubusercontent.com/jmnote/z-icons/master/svg/twitter.svg' alt='Twitter logo' style={{ height: 50, marginTop: 20, marginLeft: 20 }}/></a>
             <a href='https://www.facebook.com/hakon.sunde.313/' target='_blank' rel='noreferrer'><img src='https://raw.githubusercontent.com/jmnote/z-icons/master/svg/facebook.svg' alt='Facebook logo' style={{ height: 50, marginTop: 20, marginLeft: 20 }}/></a>
         </Box>
-        <img src='/images/flight-reacts-removebg-preview.png' alt='Selfie of famous streamer Flight Reacts' style={{ maxWidth: '25%', height: 'auto', minHeight: 300 }}/>
         </Box>
     );
 };
