5d2bf09f1086f7a29fc2d3a00828ee11cc667df2
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-09 01:43:57 +0200

    Change blog design plus rwd

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,4 @@
-import { Box, Typography, TextField, IconButton, Snackbar, Grid, Paper, Modal } from '@mui/material';
+import { Box, Typography, TextField, IconButton, Snackbar, Grid, Paper, Modal, useMediaQuery } from '@mui/material';
 import LoginButton from '../login';
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -13,65 +13,67 @@ const Blog = () => {
     const [selectedBlog, setSelectedBlog] = useState(null);
     const [commentContent, setCommentContent] = useState('');
     const [open, setOpen] = useState(false);
-  
+    const isMobile = useMediaQuery('(max-width:600px)');
+
     useEffect(() => {
         const fetchBlog = async () => { 
-          try {
-              const response = await axios.get('https://portfolioapi-hysa.onrender.com/api/Blogs');
-              setBlog(response.data);
-          } catch (error) {
-              console.error('Error fetching orders', error);
-          }
-        };
-    
-        const fetchComment = async () => {
-          try {
-            if (user) {
-              const response = await axios.get('https://portfolioapi-hysa.onrender.com/api/Comments');
-              setComment(response.data);
+            try {
+                const response = await axios.get('https://portfolioapi-hysa.onrender.com/api/Blogs');
+                setBlog(response.data);
+            } catch (error) {
+                console.error('Error fetching orders', error);
             }
-          } catch (error) {
-            console.error('Error fetching comments', error);
-          }
         };
-    
+
+        const fetchComment = async () => {
+            try {
+                if (user) {
+                    const response = await axios.get('https://portfolioapi-hysa.onrender.com/api/Comments');
+                    setComment(response.data);
+                }
+            } catch (error) {
+                console.error('Error fetching comments', error);
+            }
+        };
+
         fetchBlog();
         fetchComment();
-      }, [user, setBlog, setComment]);
-    
-      const handleSubmitComment = async () => {
+    }, [user, setBlog, setComment]);
+
+    const handleSubmitComment = async () => {
         try {
-          const userComments = comments.filter(comment => comment.userId === user?.sub);
-          const maxCommentsPerUser = 10; 
-          if (userComments.length >= maxCommentsPerUser) {
-            console.log('You have reached the maximum comment limit.');
-            setOpen(true);
-            return;
-          }
-          const response = await axios.post('https://portfolioapi-hysa.onrender.com/api/Comments', {
-            postId: selectedBlog?._id,
-            userId: user?.sub,
-            userPicture: user?.picture,
-            userName: user?.nickname,
-            content: commentContent,
-            date: new Date().toISOString(),
-        });
-    
-        setComment([...comments, response.data]);
-        setCommentContent('');
-      } catch (error) {
-        console.error('Error submitting comment:', error);
-      }};
-    
-      const handleSnackbarClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
+            const userComments = comments.filter(comment => comment.userId === user?.sub);
+            const maxCommentsPerUser = 10; 
+            if (userComments.length >= maxCommentsPerUser) {
+                console.log('You have reached the maximum comment limit.');
+                setOpen(true);
+                return;
+            }
+            const response = await axios.post('https://portfolioapi-hysa.onrender.com/api/Comments', {
+                postId: selectedBlog?._id,
+                userId: user?.sub,
+                userPicture: user?.picture,
+                userName: user?.nickname,
+                content: commentContent,
+                date: new Date().toISOString(),
+            });
+
+            setComment([...comments, response.data]);
+            setCommentContent('');
+        } catch (error) {
+            console.error('Error submitting comment:', error);
         }
-    
+    };
+
+    const handleSnackbarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
         setOpen(false);
-      }
-    
-      const confirmDelete = (
+    }
+
+    const confirmDelete = (
         <React.Fragment>
             <IconButton
                 size='small'
@@ -83,103 +85,119 @@ const Blog = () => {
             </IconButton>
         </React.Fragment>
     )
-    
-      const handleOpen = (blog) => {
+
+    const handleOpen = (blog) => {
         setSelectedBlog(blog);
-      };
-    
-      const handleClose = () => {
+    };
+
+    const handleClose = () => {
         setSelectedBlog(null);
-      };
-    
+    };
+
     return (
         <div>
-        <Box sx={{ backgroundColor: '#dedede', width: '100vw', height: 963 }}>
-        <Typography sx={{ textAlign: 'center', paddingTop: 4 }} variant='h4'>
-            Blog
-        </Typography>
-        <Grid container spacing={3} sx={{ marginLeft: 10, marginTop: 10, width: '90%' }} direction='row'>
-        {blogs.map((blog, index) => (
-        <Grid item key={index}>
-            <Paper sx={{ width: 400, height: 510, cursor: 'pointer', overflow: 'hidden', padding: 4 }} onClick={() => handleOpen(blog)}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 4 }}>
-                <Typography variant='h5' sx={{ marginTop: 2 }}>{blog.title}</Typography>
-                <Typography sx={{ marginTop: 2, marginRight: 2 }}>{blog.date}</Typography>
-            </Box>
-            <Typography sx={{ marginBottom: 3, overflow: 'hidden', textOverflow: 'ellipsis', display: 'inline-block', maxHeight: '25.4em', lineHeight: '1.8em' }}>{blog.content}</Typography>
-            </Paper>
-        </Grid>
-        ))}
-        </Grid>
-        <Modal open={selectedBlog !== null} onClose={handleClose}>
-            <Box 
-            sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                bgcolor: 'background.paper',
-                boxShadow: 24,
-                p: 4,
-                maxWidth: '90%',
-                maxHeight: '90%',
-                overflow: 'auto',
-                outline: 'none',
-            }}>
-            <Typography variant='h4'>{selectedBlog?.title}</Typography>
-            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', marginTop: 2 }}>
-                <Typography>written by {selectedBlog?.author}</Typography>
-                <Typography>{selectedBlog?.date}</Typography>
-            </Box>
-            <Typography sx={{ marginBottom: 3, marginTop: 2 }}>{selectedBlog?.content}</Typography>
-            <hr />
-            <Typography variant='h6' sx={{ fontWeight: 'bold' }}>Comments</Typography>
-            {user && selectedBlog?._id && (
-                <>
-                {comments
-                .filter((comment) => comment.postId === selectedBlog._id)
-                .map((comment, index) => (
-                    <Box key={index} sx={{ marginTop: 2, display: 'flex', alignItems: 'center', gap: 2 }}>
-                    <img src={comment.userPicture} alt='Profile pic' style={{ width: 37, height: 37, borderRadius: '50%' }}/>
-                    <Box>
-                    <Typography sx={{ fontWeight: 'bold' }}>{comment.userName}</Typography>
-                    <Typography>{comment.content}</Typography>
-                    </Box>
-                    </Box>
-                ))}
-                {selectedBlog?._id && comments.filter((comment) => comment.postId === selectedBlog._id).length === 0 && (
-                <Typography sx={{ marginTop: 2 }}>No comments</Typography>
-                )}  
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <TextField 
-                    variant='outlined' 
-                    label='Write comment' 
-                    sx={{ marginTop: 3, width: 730 }}
-                    value={commentContent}
-                    onChange={(e) => setCommentContent(e.target.value)}
-                />
-                <IconButton onClick={handleSubmitComment} sx={{ marginTop: 3 }}>
-                    <SendIcon />
-                </IconButton>
+            <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '90%', maxWidth: 1200 }}>
+                    <Typography sx={{ textAlign: 'center', paddingTop: 4, color: '#fcfcfc' }} variant='h4'>
+                        blog
+                    </Typography>
+                    <Grid container spacing={3} sx={{ marginTop: 3, gap: 10 }} justifyContent={isMobile ? 'center' : 'center'}>
+                        {blogs.map((blog, index) => (
+                            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                                <Paper
+                                    sx={{
+                                        width: '100%',
+                                        height: '100%',
+                                        cursor: 'pointer',
+                                        padding: 3,
+                                        borderRadius: 20,
+                                        display: 'flex',
+                                        gap: 2,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        transition: 'transform 0.2s',
+                                        '&:hover': {
+                                            transform: 'scale(1.05)',
+                                        }
+                                    }}
+                                    onClick={() => handleOpen(blog)}
+                                >
+                                    <Typography variant='h5'>{blog.title}</Typography>
+                                    <Typography>{blog.date}</Typography>
+                                </Paper>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Box>
-                </>
-            )}
-            {!user && (
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <Typography>Login to view comments</Typography>
-                <LoginButton />
-                </Box>
-            )}
             </Box>
-        </Modal>
-        </Box>
-        <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-        message='You have reached the maximum comment limit'
-        action={confirmDelete}
-        />
+            <Modal open={selectedBlog !== null} onClose={handleClose}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4,
+                        maxWidth: '90%',
+                        maxHeight: '90%',
+                        overflow: 'auto',
+                        outline: 'none',
+                    }}>
+                    <Typography variant='h4'>{selectedBlog?.title}</Typography>
+                    <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', marginTop: 2 }}>
+                        <Typography>written by {selectedBlog?.author}</Typography>
+                        <Typography>{selectedBlog?.date}</Typography>
+                    </Box>
+                    <Typography sx={{ marginBottom: 3, marginTop: 2 }}>{selectedBlog?.content}</Typography>
+                    <hr />
+                    <Typography variant='h6' sx={{ fontWeight: 'bold' }}>Comments</Typography>
+                    {user && selectedBlog?._id && (
+                        <>
+                            {comments
+                                .filter((comment) => comment.postId === selectedBlog._id)
+                                .map((comment, index) => (
+                                    <Box key={index} sx={{ marginTop: 2, display: 'flex', alignItems: 'center', gap: 2 }}>
+                                        <img src={comment.userPicture} alt='Profile pic' style={{ width: 37, height: 37, borderRadius: '50%' }} />
+                                        <Box>
+                                            <Typography sx={{ fontWeight: 'bold' }}>{comment.userName}</Typography>
+                                            <Typography>{comment.content}</Typography>
+                                        </Box>
+                                    </Box>
+                                ))}
+                            {selectedBlog?._id && comments.filter((comment) => comment.postId === selectedBlog._id).length === 0 && (
+                                <Typography sx={{ marginTop: 2 }}>No comments</Typography>
+                            )}
+                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                <TextField
+                                    variant='outlined'
+                                    label='Write comment'
+                                    sx={{ marginTop: 3, width: 730 }}
+                                    value={commentContent}
+                                    onChange={(e) => setCommentContent(e.target.value)}
+                                />
+                                <IconButton onClick={handleSubmitComment} sx={{ marginTop: 3 }}>
+                                    <SendIcon />
+                                </IconButton>
+                            </Box>
+                        </>
+                    )}
+                    {!user && (
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                            <Typography>Login to view comments</Typography>
+                            <LoginButton />
+                        </Box>
+                    )}
+                </Box>
+            </Modal>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleSnackbarClose}
+                message='You have reached the maximum comment limit'
+                action={confirmDelete}
+            />
         </div>
     );
 };
