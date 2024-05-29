6d226ba5a8e521a5c135ed2f6826c28164ec9d3c
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-29 15:40:46 +0200

    Remove Vue, import react app with typescript template

---
+++ src/assets/Header.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact']
+const Header = () => {
+    return (
+        <Box>
+
+        </Box>
+    );
+};
+
+export default Header;
