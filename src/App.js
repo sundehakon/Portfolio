f13a4e5f23962697f24833b82fdbef0c4a44507c
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-31 08:40:22 +0200

    Fix issue in routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { CssBaseline, Container } from '@mui/material';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import About from './Components/About';
@@ -12,11 +12,11 @@ function App() {
       <CssBaseline />
       <Container maxWidth="lg">
         <Header />
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Routes>
       </Container>
     </Router>
   );
