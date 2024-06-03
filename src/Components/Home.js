33213167c622e269c5d9f44d74e72f7e45cd3137
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-03 19:16:54 +0200

    Add skills

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,18 @@
-import { Typography, Container, Box, Button } from "@mui/material";
+import { Typography, Container, Box, Button, Grid } from "@mui/material";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+const skills = [
+    { name: 'JavaScript', icon: '/skill-icons/js.png' },
+    { name: 'React', icon: '/skill-icons/react.png' },
+    { name: 'Node.js', icon: '/skill-icons/nodejs.png' },
+    { name: 'C#', icon: '/skill-icons/csharp.png' },
+    { name: 'Python', icon: '/skill-icons/python.png' },
+    { name: 'HTML', icon: '/skill-icons/html.png' },
+    { name: 'CSS', icon: '/skill-icons/css.png' },
+    { name: 'Git', icon: '/skill-icons/git.png' },
+    { name: 'MongoDB', icon: '/skill-icons/mongodb.png' },
+
+];
 
 const Home = () => {
     return (
@@ -10,7 +23,7 @@ const Home = () => {
                 left: 0, 
                 width: '100%', 
                 height: '100%', 
-                background: 'linear-gradient(133deg, rgba(21,21,21,0) 11%, rgba(21,21,21,0.2889749649859944) 29%, rgba(21,21,21,0.2889749649859944) 36%, rgba(21,21,21,0) 51%)', 
+                background: 'linear-gradient(167deg, rgba(21,21,21,0) 0%, rgba(21,21,21,0.2889749649859944) 22%, rgba(21,21,21,0.21054359243697474) 41%, rgba(21,21,21,0) 56%)', 
                 zIndex: 1 
             }} />
             <Container sx={{ position: 'relative', overflow: 'hidden' }}>
@@ -33,6 +46,7 @@ const Home = () => {
                         gap: 2, 
                         textAlign: 'center' 
                     }}>
+                        {/* Change gradient color because its ugly rn */}
                         <Typography variant='h2' sx={{ 
                             marginBottom: { xs: 2, sm: 0 }, 
                             fontWeight: 'bolder',  
@@ -77,8 +91,30 @@ const Home = () => {
                         />
                     </Box>
                 </Box>
-                <Box sx={{ textAlign: 'center' }}>
+
+                <Box sx={{ textAlign: 'center', marginTop: 20 }}>
                     <Typography variant='h4'>Skillset</Typography>
+                    <Grid
+                        container
+                        spacing={3}
+                        justifyContent='center'
+                        alignItems='center'
+                        sx={{ marginTop: 3 }}
+                    >
+                        {skills.map((item) => (
+                            <Grid item key={item}>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <img src={item.icon} alt={item.name} style={{ height: 82, width: 82 }}/>
+                                </Box>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Box>
             </Container>
         </div>
