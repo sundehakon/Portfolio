d04c8f2fee0996daf71fef7b2ee79a1a7a69a219
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-03 09:47:38 +0200

    Apply styling for home elements

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,31 @@
-import { Typography, Container, Box } from "@mui/material"
+import { Typography, Container, Box } from "@mui/material";
 
 const Home = () => {
     return (
         <Container>
-            <Typography>Håkon Sunde</Typography>
-            <Box sx={{ border: '1px solid grey', borderRadius: '100%', width: 450 }}>
-                <img src='/me.png' alt='Me' style={{ height: 400, width: 400 }}/>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography>Håkon Sunde</Typography>
+                <Box sx={{ 
+                    border: 1, 
+                    borderRadius: '50%', 
+                    overflow: 'hidden', 
+                    height: 400, 
+                    width: 400,
+                    display: 'flex', 
+                    justifyContent: 'center', 
+                    alignItems: 'center' 
+                }}>
+                    <img 
+                        src='/me.png' 
+                        alt='Me' 
+                        style={{ 
+                            height: '100%', 
+                            width: '100%', 
+                            objectFit: 'cover', 
+                            objectPosition: 'top' 
+                        }}
+                    />
+                </Box>
             </Box>
         </Container>
     );
