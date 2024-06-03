34c3f9ca912a93858b0fa4f50fc129c0c6db217a
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-03 19:23:10 +0200

    Implement levitate hover effect to skillset images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,48 +11,47 @@ const skills = [
     { name: 'CSS', icon: '/skill-icons/css.png' },
     { name: 'Git', icon: '/skill-icons/git.png' },
     { name: 'MongoDB', icon: '/skill-icons/mongodb.png' },
-
 ];
 
 const Home = () => {
     return (
         <div>
-            <Box sx={{ 
-                position: 'absolute', 
-                top: 0, 
-                left: 0, 
-                width: '100%', 
-                height: '100%', 
-                background: 'linear-gradient(167deg, rgba(21,21,21,0) 0%, rgba(21,21,21,0.2889749649859944) 22%, rgba(21,21,21,0.21054359243697474) 41%, rgba(21,21,21,0) 56%)', 
-                zIndex: 1 
+            <Box sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                background: 'linear-gradient(167deg, rgba(21,21,21,0) 0%, rgba(21,21,21,0.2889749649859944) 22%, rgba(21,21,21,0.21054359243697474) 41%, rgba(21,21,21,0) 56%)',
+                zIndex: 1
             }} />
             <Container sx={{ position: 'relative', overflow: 'hidden' }}>
-                <Box sx={{ 
-                    position: 'relative', 
-                    zIndex: 2, 
-                    display: 'flex', 
-                    justifyContent: 'space-between', 
-                    alignItems: 'center', 
+                <Box sx={{
+                    position: 'relative',
+                    zIndex: 2,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
                     marginTop: 10,
-                    flexDirection: { xs: 'column', sm: 'row' }, 
+                    flexDirection: { xs: 'column', sm: 'row' },
                     textAlign: { xs: 'center', sm: 'left' },
                     padding: 3
                 }}>
-                    <Box sx={{ 
-                        position: 'relative', 
-                        zIndex: 2, 
-                        display: 'flex', 
-                        flexDirection: 'column', 
-                        gap: 2, 
-                        textAlign: 'center' 
+                    <Box sx={{
+                        position: 'relative',
+                        zIndex: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                        textAlign: 'center'
                     }}>
                         {/* Change gradient color because its ugly rn */}
-                        <Typography variant='h2' sx={{ 
-                            marginBottom: { xs: 2, sm: 0 }, 
-                            fontWeight: 'bolder',  
+                        <Typography variant='h2' sx={{
+                            marginBottom: { xs: 2, sm: 0 },
+                            fontWeight: 'bolder',
                             background: 'linear-gradient(133deg, rgba(28,28,31,1) 17%, rgba(182,182,183,1) 92%)',
                             WebkitBackgroundClip: 'text',
-                            WebkitTextFillColor: 'transparent' 
+                            WebkitTextFillColor: 'transparent'
                         }}>
                             Håkon Sunde
                         </Typography>
@@ -65,28 +64,28 @@ const Home = () => {
                                 Github
                             </Button>
                         </Box>
-                    </Box> 
-                    <Box sx={{ 
-                        borderRadius: '50%', 
-                        overflow: 'hidden', 
-                        height: 400, 
+                    </Box>
+                    <Box sx={{
+                        borderRadius: '50%',
+                        overflow: 'hidden',
+                        height: 400,
                         width: 400,
-                        minWidth: 400, 
+                        minWidth: 400,
                         maxWidth: 400,
-                        display: { xs: 'none', sm: 'flex' }, 
-                        justifyContent: 'center', 
+                        display: { xs: 'none', sm: 'flex' },
+                        justifyContent: 'center',
                         alignItems: 'center',
                         marginTop: { xs: 2, sm: 0 },
                         background: 'linear-gradient(133deg, rgba(28,28,31,1) 17%, rgba(182,182,183,1) 92%)'
                     }}>
-                        <img 
-                            src='/me.png' 
-                            alt='Memoji' 
-                            style={{ 
-                                height: '100%', 
-                                width: '100%', 
-                                objectFit: 'cover', 
-                                objectPosition: 'top' 
+                        <img
+                            src='/me.png'
+                            alt='Memoji'
+                            style={{
+                                height: '100%',
+                                width: '100%',
+                                objectFit: 'cover',
+                                objectPosition: 'top'
                             }}
                         />
                     </Box>
@@ -96,21 +95,27 @@ const Home = () => {
                     <Typography variant='h4'>Skillset</Typography>
                     <Grid
                         container
-                        spacing={3}
+                        spacing={5}
                         justifyContent='center'
                         alignItems='center'
                         sx={{ marginTop: 3 }}
                     >
-                        {skills.map((item) => (
-                            <Grid item key={item}>
+                        {skills.map((item, index) => ( 
+                            <Grid item key={index}> 
                                 <Box
                                     sx={{
                                         display: 'flex',
                                         justifyContent: 'center',
                                         alignItems: 'center',
+                                        '& img': { 
+                                            transition: 'transform 0.3s ease-in-out',
+                                        },
+                                        '& img:hover': { 
+                                            transform: 'translateY(-10px)', 
+                                        },
                                     }}
                                 >
-                                    <img src={item.icon} alt={item.name} style={{ height: 82, width: 82 }}/>
+                                    <img src={item.icon} alt={item.name} style={{ height: 82, width: 82 }} />
                                 </Box>
                             </Grid>
                         ))}
