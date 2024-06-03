8330d0eca0ae4e35b082434f723c64bb25c9227e
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-03 08:22:46 +0200

    Add memoji and starter circular border for framing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,12 @@
-import { Typography, Container } from "@mui/material"
+import { Typography, Container, Box } from "@mui/material"
 
 const Home = () => {
     return (
         <Container>
             <Typography>Håkon Sunde</Typography>
+            <Box sx={{ border: '1px solid grey', borderRadius: '100%', width: 450 }}>
+                <img src='/me.png' alt='Me' style={{ height: 400, width: 400 }}/>
+            </Box>
         </Container>
     );
 };
