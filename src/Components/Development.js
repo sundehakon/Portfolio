8fbd3eb462f439f5a21a4a1812520845f852ddb4
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-08-04 19:49:03 +0200

    Add experimental loading project to development page

--- src/Components/Development.js
+++ src/Components/Development.js
@@ -1,9 +1,29 @@
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Button, CircularProgress, Box } from '@mui/material';
+import { useState } from 'react';
 
 const Development = () => {
+    const [state, setState] = useState(true);
+
+    const handleDeactivate = () => {
+        setState(false);
+    };
+
+    const handleActivate = () => {
+        setState(true);
+    };
+
     return (
         <Container>
             <Typography>Development Area</Typography>
+            {state === true && 
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <CircularProgress />
+                    <Button onClick={handleDeactivate}>Deactivate</Button>  
+                </Box>
+            }
+            {state === false &&        
+                <Button onClick={handleActivate}>Activate</Button>
+            }
         </Container>
     );
 };
