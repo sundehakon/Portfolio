d111369c70af8826ad607f4d666da7e7b6855f89
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-09-12 13:15:20 +0200

    Add todocli project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -81,6 +81,57 @@ const Projects = () => {
             }}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
                     <Typography variant='h4' sx={{ padding: 3 }}>
+                        TODO-CLI
+                    </Typography>
+                    <Typography sx={{ padding: 3, flexGrow: 1 }}>
+                        A simple CLI todo list application. The application allows the user to add, remove, and list tasks. The tasks are stored in a JSON file, and the application is built using the Rust programming language. The program also has a motivational feature which fetches a random quote to help the user on it's journey to finish their daily chores.
+                        <br /><br />
+                        <strong>Key features:</strong>
+                        <List>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <ArrowForwardIcon />
+                                </ListItemIcon>
+                                Add, remove, and list tasks
+                            </ListItem>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <ArrowForwardIcon />
+                                </ListItemIcon>
+                                JSON file storage for tasks
+                            </ListItem>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <ArrowForwardIcon />
+                                </ListItemIcon>
+                                Motivational quotes for the user
+                            </ListItem>
+                        </List>
+                    </Typography>
+                    <Box sx={{ display: 'flex', padding: 2, gap: 2 }}>
+                        <Button endIcon={<GitHubIcon />} sx={{ color: 'rgba(0, 0, 0, 0.87)' }} style={{ backgroundColor: 'transparent' }} disableRipple href='https://github.com/sundehakon/TODO-CLI' target='_blank'>
+                            Github
+                        </Button>
+                    </Box>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                    <img src='/screenshots/todocli.png' alt='Command line interface of To-Do application' style={{ maxHeight: 400, maxWidth: '100%', objectFit: 'cover', borderRadius: 20, padding: 10 }} />
+                </Box>
+            </Box>
+            <Box sx={{
+                border: 1,
+                borderColor: 'divider',
+                borderRadius: 2,
+                boxShadow: 3,
+                display: 'flex',
+                flexDirection: { xs: 'column', md: 'row' },
+                backgroundColor: 'rgba(0, 0, 0, 0.04)',
+                overflow: 'hidden',
+                mb: 4,
+                marginTop: 4
+            }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                    <Typography variant='h4' sx={{ padding: 3 }}>
                         SundeAI
                     </Typography>
                     <Typography sx={{ padding: 3, flexGrow: 1 }}>
