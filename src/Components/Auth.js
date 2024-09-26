2b3c00a2211a88ffc351a47b14958abbc2c12dab
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-09-26 09:52:20 +0200

    Add logic and extra text to Auth

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,16 +1,27 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
 import LoginButton from "./Login";
 import LogoutButton from "./Logout";
 import { useTranslation } from 'react-i18next';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Auth = () => {
     const { t } = useTranslation();
+    const { isAuthenticated } = useAuth0();
 
     return (
         <Container>
-            <Typography variant="h4">{t('authtitle')}</Typography>
-            <LoginButton />
-            <LogoutButton />
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: 10 }}>
+                <Typography variant='h4' sx={{ textAlign: 'center' }}>{t('authtitle')}</Typography>
+                {!isAuthenticated &&
+                    <Box sx={{ textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 20 }}>
+                        <LoginButton />
+                        <Typography>Login to view profile card</Typography>
+                    </Box>
+                }
+                {isAuthenticated &&
+                    <LogoutButton />
+                }
+            </Box>
         </Container>
     );
 };
