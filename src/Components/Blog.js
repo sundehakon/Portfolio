dea7ccd9edaacc146ee8508be3be1dea8aaa517e
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-10-09 10:04:42 +0200

    Center loading circle

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -109,7 +109,9 @@ const Blog = () => {
                         </Box>
                     ))
                 ) : (
-                    <CircularProgress />
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                        <CircularProgress />
+                    </Box>
                 )}
                 {isAuthenticated && comments.length > 0 ? (
                     comments.map((comment, index) => (
