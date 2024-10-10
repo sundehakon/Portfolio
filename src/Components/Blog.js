c64bdab8ef7c6e05d56d91dc00698656f86f3854
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-10-10 12:09:34 +0200

    Add gradient background to blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -94,143 +94,154 @@ const Blog = () => {
     };
 
     return (
-        <Container>
-            <Box sx={{ 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'flex-start', 
-                flexWrap: 'wrap',  
-                gap: { xs: 2, sm: 3, md: 5 } 
-            }}>
-                {blogs.length > 0 ? (
-                    blogs.map((blog, index) => (
-                        <Box key={index} sx={{ 
-                            width: { xs: '100%', sm: '80%', md: '45%' },  
-                            display: 'flex', 
-                            justifyContent: 'center', 
-                            alignItems: 'center', 
-                            position: 'relative', 
-                            mt: 2, 
-                            marginBottom: 10 
-                        }}>
-                            <Paper sx={{ 
-                                width: '100%',  
-                                borderRadius: 3, 
-                                overflow: 'hidden', 
-                                marginTop: { xs: 5, sm: 5, md: 8 },
-                                transition: '0.3s',
-                                boxShadow: 3 
-                            }} elevation={2}>
-        
-                                <Box sx={{ 
-                                    background: 'linear-gradient(133deg, rgba(49,92,138,1) 17%, rgba(91,162,240,1) 92%)', 
-                                    height: { xs: 200, sm: 250, md: 300 },  
-                                    display: 'flex', 
-                                    justifyContent: 'center', 
-                                    alignItems: 'center',
-                                    padding: { xs: 2, md: 3 }  
-                                }}>
-                                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: 3, textAlign: 'center' }}>
-                                        <Typography sx={{ color: 'white', fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' } }}>{blog.title}</Typography>
-                                        <Typography sx={{ color: 'white', fontSize: { xs: '0.9rem', sm: '1rem', md: '1.2rem' } }}>- {blog.author}</Typography>
-                                        <Typography sx={{ color: 'white', fontSize: { xs: '0.8rem', sm: '0.9rem', md: '1rem' } }}>{blog.date}</Typography>
-                                        {isAuthenticated ? (
-                                            <IconButton onClick={() => openCommentModal(blog)}>
-                                                <CommentOutlinedIcon sx={{ color: 'white' }} />
-                                            </IconButton>
-                                        ) : (
-                                            <Tooltip title="Log in to view comments">
-                                                <span>
-                                                    <IconButton onClick={() => openCommentModal(blog)} disabled={!isAuthenticated}>
-                                                        <CommentOutlinedIcon sx={{ color: 'white' }} />
-                                                    </IconButton>
-                                                </span>
-                                            </Tooltip>
-                                        )}
-                                    </Box>
-                                </Box>
-                                <Divider />
-                                <Box sx={{ 
-                                    display: '-webkit-box',
-                                    WebkitLineClamp: expandedIndex === index ? 'none' : 5, 
-                                    WebkitBoxOrient: 'vertical',
+        <div>
+            <Box sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                background: 'linear-gradient(167deg, rgba(21,21,21,0) 0%, rgba(91,162,240,0.2889749649859944) 22%, rgba(91,162,240,0.3702074579831933) 41%, rgba(21,21,21,0) 56%)',
+                zIndex: -1
+            }} />
+            <Container>
+                <Box sx={{ 
+                    display: 'flex', 
+                    justifyContent: 'center', 
+                    alignItems: 'flex-start', 
+                    flexWrap: 'wrap',  
+                    gap: { xs: 2, sm: 3, md: 5 } 
+                }}>
+                    {blogs.length > 0 ? (
+                        blogs.map((blog, index) => (
+                            <Box key={index} sx={{ 
+                                width: { xs: '100%', sm: '80%', md: '45%' },  
+                                display: 'flex', 
+                                justifyContent: 'center', 
+                                alignItems: 'center', 
+                                position: 'relative', 
+                                mt: 2, 
+                                marginBottom: 10 
+                            }}>
+                                <Paper sx={{ 
+                                    width: '100%',  
+                                    borderRadius: 3, 
                                     overflow: 'hidden', 
-                                    margin: { xs: 2, md: 3 },
-                                    padding: { xs: 1, md: 2 },
-                                    wordBreak: 'break-word'  
-                                }}>
-                                    <Typography sx={{ fontSize: { xs: '0.9rem', sm: '1rem', md: '1.1rem' } }}>{blog.content}</Typography>
+                                    marginTop: { xs: 5, sm: 5, md: 8 },
+                                    transition: '0.3s',
+                                    boxShadow: 3 
+                                }} elevation={2}>
+            
+                                    <Box sx={{ 
+                                        background: 'linear-gradient(133deg, rgba(49,92,138,1) 17%, rgba(91,162,240,1) 92%)', 
+                                        height: { xs: 200, sm: 250, md: 300 },  
+                                        display: 'flex', 
+                                        justifyContent: 'center', 
+                                        alignItems: 'center',
+                                        padding: { xs: 2, md: 3 }  
+                                    }}>
+                                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: 3, textAlign: 'center' }}>
+                                            <Typography sx={{ color: 'white', fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' } }}>{blog.title}</Typography>
+                                            <Typography sx={{ color: 'white', fontSize: { xs: '0.9rem', sm: '1rem', md: '1.2rem' } }}>- {blog.author}</Typography>
+                                            <Typography sx={{ color: 'white', fontSize: { xs: '0.8rem', sm: '0.9rem', md: '1rem' } }}>{blog.date}</Typography>
+                                            {isAuthenticated ? (
+                                                <IconButton onClick={() => openCommentModal(blog)}>
+                                                    <CommentOutlinedIcon sx={{ color: 'white' }} />
+                                                </IconButton>
+                                            ) : (
+                                                <Tooltip title="Log in to view comments">
+                                                    <span>
+                                                        <IconButton onClick={() => openCommentModal(blog)} disabled={!isAuthenticated}>
+                                                            <CommentOutlinedIcon sx={{ color: 'white' }} />
+                                                        </IconButton>
+                                                    </span>
+                                                </Tooltip>
+                                            )}
+                                        </Box>
+                                    </Box>
+                                    <Divider />
+                                    <Box sx={{ 
+                                        display: '-webkit-box',
+                                        WebkitLineClamp: expandedIndex === index ? 'none' : 5, 
+                                        WebkitBoxOrient: 'vertical',
+                                        overflow: 'hidden', 
+                                        margin: { xs: 2, md: 3 },
+                                        padding: { xs: 1, md: 2 },
+                                        wordBreak: 'break-word'  
+                                    }}>
+                                        <Typography sx={{ fontSize: { xs: '0.9rem', sm: '1rem', md: '1.1rem' } }}>{blog.content}</Typography>
+                                    </Box>
+                                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                        <Button onClick={() => toggleExpand(index)} sx={{ margin: 2 }}>
+                                            {expandedIndex === index ? <Tooltip title='Collapse'><ArrowUpward /></Tooltip> : <Tooltip title='Expand'><ArrowDownward /></Tooltip>}
+                                        </Button>
+                                    </Box>
+                                </Paper>
+                            </Box>
+                        ))
+                    ) : (
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                            <CircularProgress />
+                        </Box>
+                    )}
+                    {selectedBlog && (
+                        <Modal 
+                            open={open} 
+                            onClose={closeCommentModal}
+                            sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                        >
+                            <Paper sx={{ display: 'flex', flexDirection: 'column', height: 500, width: 450, borderRadius: 3, padding: 5, gap: 4 }}>
+                                <Typography variant='h5' sx={{ textAlign: 'center' }}>Comments</Typography>
+                                <Box sx={{ flexGrow: 1, overflowY: 'auto', marginBottom: 2 }}>
+                                    {isAuthenticated && (
+                                        comments.filter(comment => comment.post_id === selectedBlog._id).length > 0 ? (
+                                            comments
+                                                .filter(comment => comment.post_id === selectedBlog._id)
+                                                .map((comment, index) => (
+                                                    <Box>
+                                                    <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: 2, marginBottom: 3, marginRight: 3 }}>
+                                                        <Avatar src={comment.user_picture} />
+                                                        <Box>
+                                                            <Typography sx={{ fontWeight: 'bold' }}>{comment.user_name}</Typography>
+                                                            <Typography>{comment.content}</Typography>
+                                                            <Typography sx={{ fontWeight: 'light' }}>{formatDate(comment.date)}</Typography>
+                                                        </Box>
+                                                        {comment.user_id === user?.sub && ( 
+                                                            <Tooltip title='Delete'>
+                                                                <IconButton onClick={() => handleDeleteComment(comment._id)} sx={{ marginLeft: 'auto', color: 'red' }}>
+                                                                    <DeleteOutlineIcon />
+                                                                </IconButton>
+                                                            </Tooltip>
+                                                        )}
+                                                    </Box>
+                                                    <Divider sx={{ margin: 2 }}/>
+                                                    </Box>
+                                                ))
+                                        ) : (
+                                            <Typography sx={{ textAlign: 'center' }}>No comments found. Be the first to comment!</Typography>
+                                        )
+                                    )}
                                 </Box>
-                                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <Button onClick={() => toggleExpand(index)} sx={{ margin: 2 }}>
-                                        {expandedIndex === index ? <Tooltip title='Collapse'><ArrowUpward /></Tooltip> : <Tooltip title='Expand'><ArrowDownward /></Tooltip>}
-                                    </Button>
+                                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                                    <TextField
+                                        variant='outlined'
+                                        label='Write comment'
+                                        fullWidth
+                                        value={commentContent}
+                                        onChange={(e) => setCommentContent(e.target.value)}
+                                    />
+                                    <Tooltip title='Send'>
+                                        <IconButton onClick={handleSubmitComment}>
+                                            <SendIcon />
+                                        </IconButton>
+                                    </Tooltip>
                                 </Box>
                             </Paper>
-                        </Box>
-                    ))
-                ) : (
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
-                        <CircularProgress />
-                    </Box>
-                )}
-                {selectedBlog && (
-                    <Modal 
-                        open={open} 
-                        onClose={closeCommentModal}
-                        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-                    >
-                        <Paper sx={{ display: 'flex', flexDirection: 'column', height: 500, width: 450, borderRadius: 3, padding: 5, gap: 4 }}>
-                            <Typography variant='h5' sx={{ textAlign: 'center' }}>Comments</Typography>
-                            <Box sx={{ flexGrow: 1, overflowY: 'auto', marginBottom: 2 }}>
-                                {isAuthenticated && (
-                                    comments.filter(comment => comment.post_id === selectedBlog._id).length > 0 ? (
-                                        comments
-                                            .filter(comment => comment.post_id === selectedBlog._id)
-                                            .map((comment, index) => (
-                                                <Box>
-                                                <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: 2, marginBottom: 3, marginRight: 3 }}>
-                                                    <Avatar src={comment.user_picture} />
-                                                    <Box>
-                                                        <Typography sx={{ fontWeight: 'bold' }}>{comment.user_name}</Typography>
-                                                        <Typography>{comment.content}</Typography>
-                                                        <Typography sx={{ fontWeight: 'light' }}>{formatDate(comment.date)}</Typography>
-                                                    </Box>
-                                                    {comment.user_id === user?.sub && ( 
-                                                        <Tooltip title='Delete'>
-                                                            <IconButton onClick={() => handleDeleteComment(comment._id)} sx={{ marginLeft: 'auto', color: 'red' }}>
-                                                                <DeleteOutlineIcon />
-                                                            </IconButton>
-                                                        </Tooltip>
-                                                    )}
-                                                </Box>
-                                                <Divider sx={{ margin: 2 }}/>
-                                                </Box>
-                                            ))
-                                    ) : (
-                                        <Typography sx={{ textAlign: 'center' }}>No comments found. Be the first to comment!</Typography>
-                                    )
-                                )}
-                            </Box>
-                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                                <TextField
-                                    variant='outlined'
-                                    label='Write comment'
-                                    fullWidth
-                                    value={commentContent}
-                                    onChange={(e) => setCommentContent(e.target.value)}
-                                />
-                                <Tooltip title='Send'>
-                                    <IconButton onClick={handleSubmitComment}>
-                                        <SendIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </Box>
-                        </Paper>
-                    </Modal>                        
-                )}
-            </Box>
-        </Container>
+                        </Modal>                        
+                    )}
+                </Box>
+            </Container>
+        </div>
     );
 };
 
