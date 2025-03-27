d69069ea05e9bee7832a191d832737929d110f22
sundehakon <142574795+sundehakon@users.noreply.github.com>
2025-03-27 14:34:15 +0100

    Remove unused import

--- src/Components/Tools/Tools.js
+++ src/Components/Tools/Tools.js
@@ -1,7 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import WorldleSolve from './WorldleSolve';
-import ArtistGuesserHelp from './ArtistGuesserHelp';
 
 const Tools = () => {
     return (
